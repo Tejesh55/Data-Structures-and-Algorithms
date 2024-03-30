43e03b2d82a13e05224e91696b7aa585793aa9a1
Tejesh <70933016+Tejesh55@users.noreply.github.com>
2024-03-30 00:52:23 -0700

    Binary_Search.java
    Implementation of Binary Search in Java

---
+++ Searching Algorithms/Binary_Search.java
@@ -0,0 +1,29 @@
+public class test{
+	public static int binarySearch(int[] nums, int target) {
+		int left=0;
+		int right=nums.length-1;
+		while(left<=right) {
+			int mid=(left+right)/2;
+			if(nums[mid]==target) {
+				return mid;
+			}
+			else if(nums[mid]<target) {
+				left=mid+1;
+			}
+			else {
+				right=mid-1;
+			}
+		}
+		return -1;
+	}
+	public static void main(String args[]) {
+		int[] nums = {1,2,3,4,5,6,7,8,9,10};
+		int target = 9;
+		int result = binarySearch(nums,target);
+		if(result!=-1) {
+			System.out.println("The target value "+ target +" exists in the array at position "+result);
+		}
+		else
+			System.out.println("The target value doesn't exist");
+	}
+}
